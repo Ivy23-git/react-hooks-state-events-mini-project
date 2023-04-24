a094d49d163d0afc33c823eb598e182e2fb186aa
ivymarubu <ivymarubu@gmail.com>
2023-04-24 23:42:30 +0300

    Trial

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
-import React, {useState} from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+import React, { useState } from "react";
 import { CATEGORIES, TASKS } from "../data";
-
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import CategoryFilter from "./CategoryFilter";
+import TaskList from "./TaskList";
+import NewTaskForm from "./NewTaskForm";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("All");
@@ -13,28 +10,34 @@ function App() {
 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
-    if (category === "All") {
-      setTasks(TASKS);
-    } else {
-      const filteredTasks = TASKS.filter((task) => task.category === category);
-      setTasks(filteredTasks);
-    }
   };
 
-  const handleTaskFormSubmit = (task) => {
-    setTasks([...tasks, task]);
+  const handleTaskFormSubmit = (newTask) => {
+    const id = tasks.length + 1;
+    setTasks([...tasks, { id, ...newTask }]);
   };
+
+  const handleTaskDelete = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const filteredTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
 
   return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter 
-      categories={[...CATEGORIES, "All"]}
-      selectedCategory={selectedCategory}
-      onSelectCategory={handleCategorySelect}
-       />
-      <NewTaskForm />
-      <TaskList />
+    <div className="app">
+      <CategoryFilter
+        categories={[...CATEGORIES, "All"]}
+        selectedCategory={selectedCategory}
+        onSelectCategory={handleCategorySelect}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={handleTaskFormSubmit}
+      />
+      <TaskList tasks={filteredTasks} onDelete={handleTaskDelete} />
     </div>
   );
 }
